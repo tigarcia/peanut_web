7cb8952d39f162f019c91d03aa7316ee78ae9240
Tim Garcia <tim.m.garcia@gmail.com>
2019-03-15 15:32:57 -0700

    Testing

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: python3 server.py
+web: uvicorn server:app
--- server.py
+++ server.py
@@ -29,23 +29,23 @@ async def download_file(url, dest):
             data = await response.read()
             with open(dest, 'wb') as f: f.write(data)
 
-async def setup_learner():
-    #await download_file(export_file_url, path/export_file_name)
-    try:
-        learn = load_learner(path, export_file_name)
-        return learn
-    except RuntimeError as e:
-        if len(e.args) > 0 and 'CPU-only machine' in e.args[0]:
-            print(e)
-            message = "\n\nThis model was trained with an old version of fastai and will not work in a CPU environment.\n\nPlease update the fastai library in your training environment and export your model again.\n\nSee instructions for 'Returning to work' at https://course.fast.ai."
-            raise RuntimeError(message)
-        else:
-            raise
-
-loop = asyncio.get_event_loop()
-tasks = [asyncio.ensure_future(setup_learner())]
-learn = loop.run_until_complete(asyncio.gather(*tasks))[0]
-loop.close()
+# async def setup_learner():
+#     #await download_file(export_file_url, path/export_file_name)
+#     try:
+#         learn = load_learner(path, export_file_name)
+#         return learn
+#     except RuntimeError as e:
+#         if len(e.args) > 0 and 'CPU-only machine' in e.args[0]:
+#             print(e)
+#             message = "\n\nThis model was trained with an old version of fastai and will not work in a CPU environment.\n\nPlease update the fastai library in your training environment and export your model again.\n\nSee instructions for 'Returning to work' at https://course.fast.ai."
+#             raise RuntimeError(message)
+#         else:
+#             raise
+#
+# loop = asyncio.get_event_loop()
+# tasks = [asyncio.ensure_future(setup_learner())]
+# learn = loop.run_until_complete(asyncio.gather(*tasks))[0]
+# loop.close()
 
 @app.route('/')
 def index(request):
